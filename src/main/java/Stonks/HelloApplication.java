0f4ab9c99861c9b61549261d85681eb68dca91b8
arfbllh <arfbllh@gmail.com>
2022-04-12 20:08:25 +0600

    database created

--- src/main/java/Stonks/HelloApplication.java
+++ src/main/java/Stonks/HelloApplication.java
@@ -4,11 +4,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import Database.DB;
 
 public class HelloApplication extends Application
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException {
 
         Stonk();
         //UserData.showAllUsers();
@@ -20,6 +22,9 @@ public class HelloApplication extends Application
     public void start(Stage primaryStage) throws Exception
     {
         LoginPage.display();
+
+
+
     }
     static public void Stonk()  {
         EntryData.init();
